d486825c87cb1963aa3bc02f05f449c04078e90f
Ravi <Ravi@DESKTOP-5A7HVVO>
2021-08-15 00:23:21 +0530

    first commit

--- src/main/java/com/olive/writer/MyItemWriter.java
+++ src/main/java/com/olive/writer/MyItemWriter.java
@@ -6,7 +6,6 @@ import org.springframework.batch.item.ItemWriter;
 
 public class MyItemWriter implements ItemWriter<String> {
 	
-	@Override
 	public void write(List<? extends String> items) throws Exception {
 	     System.out.println("Writer executed  ......");
 		System.out.println("Data is ::>" + items);
